64f60856ee199a4cd1fa1de13b43920eee08c120
eusantosdev <eusantosdev@gmail.com>
2025-05-21 21:30:05 -0300

    feat: Exemplo do funcionamento do Estoque

--- src/CalculadoraDeEstoque.java
+++ src/CalculadoraDeEstoque.java
@@ -1,10 +1,19 @@
 public class CalculadoraDeEstoque {
     public static void main(String[] args) {
-        double livroJava8 = 59.90;
-        double livroTDD = 59.90;
+        double soma = 0;
 
-        double soma = livroJava8 + livroTDD;
+        for (double i = 0; i < 35; i++) {
+            soma += 59.90;
+        }
 
         System.out.println("O total em estoque é R$"+soma);
+
+        if (soma < 150) {
+            System.out.println("Seu estoque está muito baixo!");
+        } else if(soma >= 2000) {
+            System.out.println("Seu estoque está muito alto!");
+        } else {
+            System.out.println("Seu estoque está bom!");
+        }
     }
 }
